1a18edaa46ed6f2b0ea4c2297fb9ea837888c4f8
supriyakavathekar <supriyakavathekar20@gmail.com>
2023-06-16 10:57:25 +0200

    added Nav bar and made it as component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-      <p className='m-12'>hi from reacct</p>
-      <button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
+    <div className="app min-h-screen ">
+      
+    <Nav/>
+    <main>main</main>
+    <footer>footer</footer>
     </div>
   );
 }
