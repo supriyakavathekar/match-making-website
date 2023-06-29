609a10b8040943b3b357f2835098f496f453b525
supriyakavathekar <supriyakavathekar20@gmail.com>
2023-06-29 14:06:02 +0200

    delete one icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -42,7 +42,7 @@ function Nav({user ,setuser, data, users, setMatches}) {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ion-icon style={{fontSize:'3em'}} name="heart-half-outline"></ion-icon>
-      <ion-icon style={{fontSize:'3em',color:'blue'}} name="heart-half-outline"></ion-icon>
+      
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><a>Item 1</a></li>
         <li>
