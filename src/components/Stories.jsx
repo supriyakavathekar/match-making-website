d1c3a008de883f6939df12306b564de885255aaa
supriyakavathekar <supriyakavathekar20@gmail.com>
2023-06-29 13:22:54 +0200

    add carsouel

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -3,8 +3,51 @@ import React from 'react'
 function Stories() {
   return (
     <div className='min-h-screen lg:flex flex-wrap gap-10' >
+
+
+
+<div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    <img src="https://ik.imagekit.io/lp7vvaw9o/Matrimony/happy_faces5.jpeg?updatedAt=1686827326133" className="w-small" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="https://ik.imagekit.io/lp7vvaw9o/Matrimony/happy_faces3.jpeg?updatedAt=1686827326095" className="w-small" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src="https://ik.imagekit.io/lp7vvaw9o/Matrimony/happy_faces9.jpeg?updatedAt=1686827326096" className="w-small" />
+  </div> 
+  <div id="item4" className="carousel-item w-full">
+    <img src="https://ik.imagekit.io/lp7vvaw9o/Matrimony/happy_faces2.png?updatedAt=1686827326337" className="w-small" />
+  </div>
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        <figure className='h-72'>
+        {/* <figure className='h-72'>
             <img src="https://ik.imagekit.io/lp7vvaw9o/Matrimony/happy_faces5.jpeg?updatedAt=1686827326133" alt="" />
         </figure>
 
@@ -30,7 +73,7 @@ function Stories() {
 
         <figure>
             <img src="https://ik.imagekit.io/lp7vvaw9o/Matrimony/happy_faces4.jpeg?updatedAt=1686827325907" alt="" />
-        </figure>
+        </figure> */}
 
 
  
